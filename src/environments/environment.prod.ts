16ba55d1a829a031c0745c8db81812adb55c74a3
tillias <ternovich@gmail.com>
2018-09-24 14:50:33 +0200

    Switched to Heroku due to Openshift Starter instability

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://openjdk-app-url-shortener1.1d35.starter-us-east-1.openshiftapps.com'
+  apiUrl: 'https://url-shortener-tillias.herokuapp.com/'
 };
